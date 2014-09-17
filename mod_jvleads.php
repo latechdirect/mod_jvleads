4f5bdcbce6a2e7a816f407cc5e8167b92bfb7fc7
Kevin Norman <webmaster@latechdirect.com>
2014-09-16 19:59:56 -0700

    Changed getLayoutPath
    Changed JMofuleHelper::getLayoutPatch parameter from mod_helloworld to
    mod_jvleads

--- mod_jvleads.php
+++ mod_jvleads.php
@@ -18,5 +18,5 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 require_once( dirname(__FILE__).'/helper.php' );
  
 $hello = modHelloWorldHelper::getHello( $params );
-require( JModuleHelper::getLayoutPath( 'mod_helloworld' ) );
+require( JModuleHelper::getLayoutPath( 'mod_jvleads' ) );
 ?>
